606109de81a7380206c9526b754b10d8ff6e1a65
Koustav Chowdhury <kc99.kol@gmail.com>
2019-02-19 01:15:31 +0530

    Check it!

--- fft.cpp
+++ fft.cpp
@@ -3,48 +3,48 @@
 using namespace std;
 vector<complex<double> > fft(vector<complex<double> > u,int n)
 {
-int c,k;
-double re,img;
-complex<double>cpx(1,0);
-complex<double>w(cos(2*pi/n),sin(2*pi/n));
-vector<complex<double> >even,odd,even_fft,odd_fft,y;
-vector<complex<double> >::iterator j,j1,j2;
-if(n==1)
-return u;
-else
-{
-c=0;
-for(j=u.begin();j!=u.end();j++)
-{
-if(c%2==0)
-even.push_back(complex<double>(real(*j),imag(*j)));
-else
-odd.push_back((complex<double>(real(*j),imag(*j))));
-c++;
-}
-even_fft=fft(even,n/2);
-odd_fft=fft(odd,n/2);
-j1=even_fft.begin();j2=odd_fft.begin();
-for(k=1;k<=n/2;k++)
-{
-re=real(*(j1)+cpx*(*(j2)));
-img=imag(*(j1)+cpx*(*(j2)));
-y.push_back(complex<double>(re,img));
-j1++;j2++;
-cpx*=w;
-}
-complex<double>cx(1,0);
-j1=even_fft.begin();j2=odd_fft.begin();
-for(k=1;k<=n/2;k++)
-{
-re=real(*(j1)-cx*(*(j2)));
-img=imag(*(j1)-cx*(*(j2)));
-y.push_back(complex<double>(re,img));
-j1++;j2++;
-cx*=w;
-}
-return y;
-}
+      int c,k;
+      double re,img;
+      complex<double>cpx(1,0);
+      complex<double>w(cos(2*pi/n),sin(2*pi/n));
+      vector<complex<double> >even,odd,even_fft,odd_fft,y;
+      vector<complex<double> >::iterator j,j1,j2;
+      if(n==1)
+        return u;
+      else
+      {
+        c=0;
+        for(j=u.begin();j!=u.end();j++)
+        {
+          if(c%2==0)
+           even.push_back(complex<double>(real(*j),imag(*j)));
+         else
+            odd.push_back((complex<double>(real(*j),imag(*j))));
+          c++;
+        }
+        even_fft=fft(even,n/2);
+        odd_fft=fft(odd,n/2);
+        j1=even_fft.begin();j2=odd_fft.begin();
+        for(k=1;k<=n/2;k++)
+        {
+          re=real(*(j1)+cpx*(*(j2)));
+          img=imag(*(j1)+cpx*(*(j2)));
+          y.push_back(complex<double>(re,img));
+          j1++;j2++;
+          cpx*=w;
+        }
+        complex<double>cx(1,0);
+        j1=even_fft.begin();j2=odd_fft.begin();
+        for(k=1;k<=n/2;k++)
+        {
+          re=real(*(j1)-cx*(*(j2)));
+          img=imag(*(j1)-cx*(*(j2)));
+          y.push_back(complex<double>(re,img));
+          j1++;j2++;
+          cx*=w;
+        }
+        return y;
+      }
 }
 
 vector<complex<double> > inv_fft(vector<complex<double> > u,int n)
@@ -93,8 +93,8 @@ for(j=y.begin();j!=y.end();j++)
 {
 re=real(*j);
 img=imag(*j);
-re=re/n;
-img=img/n;
+re=re/(double)n;
+img=img/(double)n;
 z.push_back(complex<double>(re,img));
 }
 return z;
@@ -129,10 +129,10 @@ for(j=p.begin();j!=p.end();j++)
 r.push_back(complex<double>(real((*(j))*(*(l))),imag((*(j))*(*(l)))));
 l++;
 }
-vector<complex<double> >ans=inv_fft(r,2*n);
+vector<complex<double> >ans=inv_fft(r,(int)r.size());
 cout<<"The product of the polynomials is\n";
 for(j=ans.begin();j!=ans.end();j++)
-cout<<*j<<" ";
+  cout<<*j<<" ";
 cout<<"\n";
 return 0;
 }
